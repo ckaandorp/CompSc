8ff34bff7f7f6b57e39f5df08f3524d425073909
topteulen <tobiasteule@gmail.com>
2020-01-08 10:48:25 +0100

    test code for mesa

---
+++ test.py
@@ -0,0 +1,61 @@
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+import numpy as np
+import matplotlib.pyplot as plt
+class MoneyModel(Model):
+	"""A model with some number of agents."""
+	def __init__(self, N, width, height):
+		self.num_agents = N
+		self.grid = MultiGrid(width, height, True)
+		self.schedule = RandomActivation(self)
+		# Create agents
+		for i in range(self.num_agents):
+			a = MoneyAgent(i, self)
+			self.schedule.add(a)
+			# Add the agent to a random grid cell
+			x = self.random.randrange(self.grid.width)
+			y = self.random.randrange(self.grid.height)
+			self.grid.place_agent(a, (x, y))
+
+	def step(self):
+		self.schedule.step()
+
+class MoneyAgent(Agent):
+	""" An agent with fixed initial wealth."""
+	def __init__(self, unique_id, model):
+		super().__init__(unique_id, model)
+		self.wealth = 1
+
+	def move(self):
+		possible_steps = self.model.grid.get_neighborhood(
+			self.pos,
+			moore=True,
+			include_center=False)
+		new_position = self.random.choice(possible_steps)
+		self.model.grid.move_agent(self, new_position)
+
+	def give_money(self):
+		cellmates = self.model.grid.get_cell_list_contents([self.pos])
+		if len(cellmates) > 1:
+			other = self.random.choice(cellmates)
+			other.wealth += 1
+			self.wealth -= 1
+
+	def step(self):
+		self.move()
+		if self.wealth > 0:
+			self.give_money()
+model = MoneyModel(50, 10, 10)
+for i in range(20):
+	model.step()
+
+
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for cell in model.grid.coord_iter():
+	cell_content, x, y = cell
+	agent_count = len(cell_content)
+	agent_counts[x][y] = agent_count
+plt.imshow(agent_counts, interpolation='nearest')
+plt.colorbar()
+plt.show()
