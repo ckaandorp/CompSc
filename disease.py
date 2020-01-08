1df2267b13239f7f6f5d74582fedd86448212fc7
topteulen <tobiasteule@gmail.com>
2020-01-08 16:42:03 +0100

    sad times

--- disease.py
+++ disease.py
@@ -1,6 +1,7 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import SingleGrid
+from mesa.datacollection import DataCollector
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -40,20 +41,22 @@ class DiseaseModel(Model):
 			# Add the agent to a random grid cell
 			location = self.grid.find_empty()
 			self.grid.place_agent(a, location)
-
+		self.datacollector = DataCollector(
+			model_reporters={"Gini": "grid"},
+			agent_reporters={"Disease": "disease"})
 	# Continue one step in simulation
 	def step(self):
 		self.schedule.step()
-
+		self.datacollector.collect(self)
 class DiseaseAgent(Agent):
-	""" An agent with fixed initial wealth."""
+	""" An agent with fixed initial disease."""
 	def __init__(self, unique_id, sociability, model):
 		super().__init__(unique_id, model)
 		# Randomly set agent as healthy or sick
 		self.disease = self.random.randrange(2)
 		self.diseaserate = 0.7
 		self.sociability = sociability
-		print(self.disease)
+		self.resistent = 0
 
 	def move(self):
 		""" Moves agent one step on the grid."""
@@ -72,14 +75,18 @@ class DiseaseAgent(Agent):
 		if len(cellmates) > 1:
 			for i in range(len(cellmates)):
 				other = cellmates[i]
-				if self.diseaserate > self.random.random():
-					other.disease = 1
+				if other.resistent == 0:
+					if self.diseaserate > self.random.random():
+						other.disease = 1
 	def cured(self):
-		if 0.4 > self.random.random():
+		if 1 > self.random.random():
 			self.disease = 0
+			self.resistent = 1
+			print(self.resistent)
 	def step(self):
 		"""Move and spread disease if sick."""
 		self.move()
+		print(self.disease)
 		if self.disease == 1:
 			self.spread_disease()
 			self.cured()
@@ -87,15 +94,28 @@ class DiseaseAgent(Agent):
 
 
 model = DiseaseModel(10, 10, 10, 10, 10)
-for i in range(1):
+for i in range(200):
 	model.step()
 
+test = model.datacollector.get_model_vars_dataframe()
+grid = test['Gini'][199]
+print(grid)
 
 agent_counts = np.zeros((model.grid.width, model.grid.height))
-for cell in model.grid.coord_iter():
+for cell in grid.coord_iter():
 	agent, x, y = cell
 	if agent != None:
 		agent_counts[x][y] = agent.disease
+	else:
+		agent_counts[x][y] = -1
+plt.imshow(agent_counts, interpolation='nearest')
+plt.colorbar()
+plt.show()
+
+for cell in model.grid.coord_iter():
+	agent, x, y = cell
+	if agent != None:
+		agent_counts[x][y] = agent.resistent
 	else:
 		agent_counts[x][y] = -1
 plt.imshow(agent_counts, interpolation='nearest')
