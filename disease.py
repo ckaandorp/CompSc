6889e7fdaa5c3e9f57db78a16f44bb4f59b8ddb4
topteulen <tobiasteule@gmail.com>
2020-01-08 11:20:22 +0100

    temp

--- disease.py
+++ disease.py
@@ -26,31 +26,34 @@ class MoneyAgent(Agent):
 	""" An agent with fixed initial wealth."""
 	def __init__(self, unique_id, model):
 		super().__init__(unique_id, model)
-		self.random.randrange(2) = 1
+		self.disease = self.random.randrange(2)
+		print(self.disease)
 
 	def move(self):
 		possible_steps = self.model.grid.get_neighborhood(
 			self.pos,
-			moore=True,
-			include_center=False)
+			moore=False,
+			include_center=True)
 		new_position = self.random.choice(possible_steps)
 		self.model.grid.move_agent(self, new_position)
 
-	def give_money(self):
-		cellmates = self.model.grid.get_cell_list_contents([self.pos])
+	def spread_disease(self):
+		cellmates = self.model.grid.get_cell_list_contents([self.model.grid.get_neighborhood(
+			self.pos,
+			moore=False,
+			include_center=True)])
 		if len(cellmates) > 1:
 			other = self.random.choice(cellmates)
-			other.wealth += 1
-			self.wealth -= 1
+			other.disease = 1
 
 	def step(self):
 		self.move()
-		if self.wealth > 0:
-			self.give_money()
+		if self.disease == 1:
+			self.spread_disease()
 
 
 
-model = MoneyModel(50, 10, 10)
+model = DiseaseModel(50, 10, 10)
 for i in range(20):
 	model.step()
 
