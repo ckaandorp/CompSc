0e96fa66739e314116f7eb6c544a72f07ed2d18f
charlottekaandorp <ckaandorp@gmail.com>
2020-01-13 13:18:20 +0100

    lowered infection prob if not direct neighbor

--- disease.py
+++ disease.py
@@ -30,7 +30,7 @@ class DiseaseModel(Model):
 		self.addAgents(highS, lowS + highS, 2)
 
 	def addAgents(self, n, startID, sociability):
-		#add n amount of agents with a sociability
+		# add n amount of agents with a sociability
 		for i in range(n):
 			a = DiseaseAgent(i + startID, sociability, self)
 			self.schedule.add(a)
@@ -59,28 +59,33 @@ class DiseaseAgent(Agent):
 		""" Moves agent one step on the grid."""
 		cellmates = self.model.grid.get_neighbors(self.pos, moore=True)
 
-		#behavior based on sociability.
+		# behavior based on sociability.
+		# move away from agent if low sociability
 		if self.sociability == 0:
 			if len(cellmates) > 0:
 				other = self.random.choice(cellmates)
+				# find escape route
 				escape = ((self.pos[0] - other.pos[0]), (self.pos[1] - other.pos[1]))
 				choice = (escape[0] + self.pos[0], escape[1] + self.pos[1])
 				if self.model.grid.width > choice[0] > 0 and self.model.grid.height > choice[1] > 0:
 					if model.grid.is_cell_empty(choice):
 						self.model.grid.move_agent(self, choice)
 						return
+		# stop if talked to if middle sociability
 		if self.sociability == 1:
 			for neighbor in cellmates:
 				if neighbor.sociability == 2:
 					return
+		# stop to talk if there is a neighbor if high sociability
 		if self.sociability == 2:
 			if len(cellmates) > 0:
 				return
 
-		#goal based movement
+		# goal based movement
 		x_distance = self.goal[0] - self.pos[0]
 		y_distance = self.goal[1] - self.pos[1]
-		#takes a step in the direction that is farthest off the current position.
+		# takes a step in the direction that is farthest off the current position.
+		# if more horizontal distance needs to be traveled.
 		if abs(x_distance) >= abs(y_distance):
 			if x_distance > 0:
 				choice = (self.pos[0]+1,self.pos[1])
@@ -90,6 +95,7 @@ class DiseaseAgent(Agent):
 				choice = (self.pos[0]-1,self.pos[1])
 				if 0 < choice[0] < model.grid.width and  0 < choice[1] < model.grid.height and model.grid.is_cell_empty(choice):
 					self.model.grid.move_agent(self,choice)
+		# if more vertical distance needs be traveled.
 		else:
 			if y_distance > 0:
 				choice = (self.pos[0],self.pos[1]+1)
@@ -107,8 +113,14 @@ class DiseaseAgent(Agent):
 			for i in range(len(cellmates)):
 				other = cellmates[i]
 				if self.disease not in other.resistent:
-					if self.diseaserate > self.random.random():
-						other.disease = self.disease
+					# if direct neighbor then normal infection probability, else lowered.
+					if (abs(self.pos[0] - other.pos[0]) + abs(self.pos[1] - other.pos[1])) == 1:
+						if self.diseaserate > self.random.random():
+							other.disease = self.disease
+					else:
+						if (self.diseaserate * 0.75) > self.random.random():
+							other.disease = self.disease
+
 	def mutate(self):
 		"""mutates disease in an agent."""
 		if self.disease > 0:
@@ -117,7 +129,7 @@ class DiseaseAgent(Agent):
 
 	def cured(self):
 		"""Cure agents based on cure probability sick time."""
-		if self.sickTime > 6:
+		if self.sickTime > 10080: # people are generally sick for at least 1 week (60 * 24 * 7)
 			if self.cureProb > self.random.random():
 				self.resistent += [self.disease]
 				self.disease = 0
@@ -135,7 +147,6 @@ class DiseaseAgent(Agent):
 			self.mutate()
 			self.spread_disease()
 			self.cured()
-
 
 
 model = DiseaseModel(10, 10, 10, 50, 50,[(0,0),(12,25),(50,50)])
