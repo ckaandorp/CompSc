100830671c06922fbdc372fc84be57eca87f3a87
charlottekaandorp <ckaandorp@gmail.com>
2020-01-28 21:31:22 +0100

    cleanup + commented

--- diseaseAgent.py
+++ diseaseAgent.py
@@ -14,13 +14,13 @@ class DiseaseAgent(Agent):
     """
     def __init__(self, unique_id, sociability, model, disease, roster):
         super().__init__(unique_id, model)
-        self.id = unique_id  # weh
+        self.id = unique_id
         self.disease = disease
         self.sociability = sociability
         self.resistent = []
         self.cureProb = self.model.initialCureProb
         self.sickTime = 0
-        self.talking = 0.1
+        self.talking = 0.1  # probability that an agent stop talking
         self.path = []
         self.goal = 0
         self.talkedto = False
@@ -28,7 +28,7 @@ class DiseaseAgent(Agent):
 
     def random_move(self):
         """
-        Moves agent one step on the grid.
+        Moves agent one step on the grid freely (non-educational setting).
         """
         possible_steps = self.model.grid.get_neighborhood(
             self.pos,
@@ -45,21 +45,28 @@ class DiseaseAgent(Agent):
 
     def move(self):
         """
-        Moves agent one step on the grid.
+        Moves agent one step on the grid according to schedule
+        (educational setting).
         """
+        # End of schoolday, move towards exit
         if self.model.counter % 1440 == 1200:
             self.goal = self.model.exit
             self.path = []
+        # Last class, move towards final scheduled classroom
         elif self.model.counter % 1440 == 940:
             self.goal = self.roster[2]
             self.path = []
+        # Second class, move towards second scheduled classroom
         elif self.model.counter % 1440 == 700:
             self.goal = self.roster[1]
             if self.pos != self.goal:
                 self.path = []
+        # First class, move towards first scheduled classroom.
         elif 700 > self.model.counter % 1440 > 540:
             self.goal = self.roster[0]
             self.path = []
+
+        # While trying to reach goal, interact with neighboring agents
         if self.pos != self.goal:
             if not isinstance(self, wall):
                 cellmates = self.model.grid.get_neighbors(self.pos, moore=True)
@@ -87,7 +94,8 @@ class DiseaseAgent(Agent):
                                 return
 
                 # stop if talked to if middle sociability
-                if self.sociability == 1 and self.random.random() > self.talking:
+                if self.sociability == 1 and \
+                   self.random.random() > self.talking:
                     for neighbor in newCellmates:
                         if neighbor.sociability == 2:
                             if self.talkedto != neighbor:
@@ -102,12 +110,13 @@ class DiseaseAgent(Agent):
                    self.random.random() > self.talking:
                     if len(newCellmates) > 0 and \
                        self.talkedto != newCellmates[0]:
-                        self.talking *= 1.5
+                        self.talking *= 1.5  # lowers prob to keep talking
                         self.talkedto = newCellmates[0]
                         return
                 else:
                     self.talking = 0.1
 
+                # use A* to find best path to goal if goal is present
                 if self.path == []:
                     self.path = AStarSearch(self.pos, self.goal, self.model)
                 if self.path != []:
@@ -123,6 +132,7 @@ class DiseaseAgent(Agent):
         """
         Spreads disease to neighbors.
         """
+        # Collect cellmates that disease can be spread to, sorted by distance
         cellmates = set(self.model.grid.get_neighbors(self.pos, moore=True))
         cellmates_2 = set(self.model.grid.get_neighbors(self.pos, moore=True,
                                                         radius=2))
@@ -134,7 +144,7 @@ class DiseaseAgent(Agent):
         cellmates_2 = list(cellmates_2.difference(cellmates))
         cellmates_3 = list(cellmates_3.difference(cellmates_2))
         cellmates_4 = list(cellmates_4.difference(cellmates_3))
-        # Check if there are neighbors to spread disease to
+        # cellmates that are further away have a lower prob of getting sick
         disease_spreader(cellmates, self, 1)
         disease_spreader(cellmates_2, self, 0.75)
         disease_spreader(cellmates_3, self, 0.5)
@@ -144,6 +154,7 @@ class DiseaseAgent(Agent):
         """
         Mutates disease in an agent.
         """
+        # Check if agent has disease to mutate
         if self.disease > 0:
             if self.model.mutateProb > self.random.random():
                 self.model.maxDisease += 1
@@ -152,6 +163,7 @@ class DiseaseAgent(Agent):
                 self.sickTime = 0
 
     def go_home(self):
+        """Remove agent from the grid as they 'go home'."""
         if self.pos == self.model.exit:
             self.model.removed += [self]
             self.model.grid.remove_agent(self)
@@ -173,16 +185,22 @@ class DiseaseAgent(Agent):
 
     def step(self):
         """Move and spread disease if sick."""
-        if self.model.counter % 1440 > 540 and self.pos is None and self.model.edu_setting is True:
+        # start of schoolday, set agent on grid at entrance (if edu setting)
+        if self.model.counter % 1440 > 540 and self.pos is None and \
+           self.model.edu_setting is True:
+            # place agent if entrance is not occupied by other agent
             if self.model.grid.is_cell_empty(self.model.exit):
                 self.model.grid.place_agent(self, self.model.exit)
+        # move agent according to setting as long as it is on grid
         if self.pos is not None:
             if self.model.edu_setting is False:
                 self.random_move()
             else:
                 self.move()
+                # end of schoolday, move towards entrance/exit and off the grid
                 if self.model.counter % 1440 > 800:
                     self.go_home()
+        # if agent carries disease, potentially spread, mutate and/or heal
         if self.disease >= 1:
             self.sickTime += 1
             self.mutate()
